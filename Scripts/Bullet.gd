00ecd5abe3ed435f26f60c718ff9f4575c5d6ccf
Lizzard <luis.codemaker@gmail.com>
2022-07-18 18:31:33 -0300

    game

--- Scripts/Bullet.gd
+++
@@ -1,8 +0,0 @@
-extends KinematicBody
-
-const SPEED = 600
-
-func _physics_process(_delta):
-	move_and_slide(transform.basis.z * -SPEED)
-	if transform.origin.z < -1000 or abs(transform.origin.x) > 60:
-		queue_free()
--- Scripts/World.gd
+++
@@ -1,7 +0,0 @@
-extends Spatial
-
-var score = 0
-onready var score_label = $CanvasLayer/RichTextLabel
-
-func _process(delta):
-	score_label.text = str(score) + " Pts"
